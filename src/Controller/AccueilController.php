d9625debe701aefa2f588be6d826f8009e001012
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2025-02-28 12:04:27 +0100

    Ajout de la table Présentation

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PresentationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AccueilController extends AbstractController
 {
     #[Route('/', name: 'portfolio_accueil')]
-    public function index(): Response
+    public function index(PresentationRepository $presentation): Response
     {
+        $presentation = $presentation->findAll();
+
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'presentation' => $presentation,
         ]);
     }
 }
