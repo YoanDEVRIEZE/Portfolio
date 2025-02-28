841db8c40344280d5a65e4544652362939e6c8c0
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2025-02-28 13:00:56 +0100

    Ajout de la table skill

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\PresentationRepository;
 use App\Repository\ProjetRepository;
+use App\Repository\SkillRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,14 +12,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AccueilController extends AbstractController
 {
     #[Route('/', name: 'portfolio_accueil')]
-    public function index(PresentationRepository $presentation, ProjetRepository $projet): Response
+    public function index(PresentationRepository $presentation, ProjetRepository $projet, SkillRepository $skill): Response
     {
         $presentation = $presentation->findAll();
         $projet = $projet->findAll();
+        $skill = $skill->findAll();
 
         return $this->render('accueil/index.html.twig', [
             'presentation' => $presentation,
             'projet' => $projet,
+            'skill' => $skill,
         ]);
     }
 }
---
+++ src/Entity/Skill.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SkillRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SkillRepository::class)]
+class Skill
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $logo = null;
+
+    #[ORM\Column]
+    private ?int $niveau = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $couleur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(string $logo): static
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    public function getNiveau(): ?int
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau(int $niveau): static
+    {
+        $this->niveau = $niveau;
+
+        return $this;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(string $couleur): static
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+}
