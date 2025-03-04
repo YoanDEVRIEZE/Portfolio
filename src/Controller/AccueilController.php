3006f6c88275843516936f127783d268f71e0a29
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2025-03-04 22:02:14 +0100

    Ajout de l'Entity Parcours + affichage du contenu

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\ContactFormType;
+use App\Repository\ParcoursRepository;
 use App\Repository\PresentationRepository;
 use App\Repository\ProjetRepository;
 use App\Repository\SkillRepository;
@@ -15,10 +16,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AccueilController extends AbstractController
 {
     #[Route('/', name: 'portfolio_accueil')]
-    public function index(Request $request, EntityManagerInterface $entityManager, PresentationRepository $presentation, ProjetRepository $projet, SkillRepository $skill, ContactFormType $form): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, PresentationRepository $presentation, ProjetRepository $projet, ParcoursRepository $parcours, SkillRepository $skill, ContactFormType $form): Response
     {
         $presentation = $presentation->findAll();
         $projet = $projet->findAll();
+        $parcours = $parcours->findBy([],['id' => 'DESC'], 3);
         $skill = $skill->findAll();
 
         $form = $this->createForm(ContactFormType::class);
@@ -40,6 +42,7 @@ final class AccueilController extends AbstractController
             return $this->render('accueil/index.html.twig', [
                 'presentation' => $presentation,
                 'projet' => $projet,
+                'parcours' => $parcours,
                 'skill' => $skill,
                 'form' => $form->createView(),
                 'section' => 4, 
@@ -49,6 +52,7 @@ final class AccueilController extends AbstractController
         return $this->render('accueil/index.html.twig', [
             'presentation' => $presentation,
             'projet' => $projet,
+            'parcours' => $parcours,
             'skill' => $skill,
             'form' => $form,
             'section' => 0, 
---
+++ src/Entity/Parcours.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ParcoursRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ParcoursRepository::class)]
+class Parcours
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $photo_couverture = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $photo = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $contenu = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPhotoCouverture(): ?string
+    {
+        return $this->photo_couverture;
+    }
+
+    public function setPhotoCouverture(string $photo_couverture): static
+    {
+        $this->photo_couverture = $photo_couverture;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): static
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): static
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+}
