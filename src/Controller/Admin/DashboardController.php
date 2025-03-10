64f0334e4a1602a7aec843d62a3a97133eb6c309
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2025-03-10 19:05:53 +0100

    Personnalisation du Dashboard EasyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use App\Entity\Parcours;
 use App\Entity\Presentation;
 use App\Entity\Projet;
 use App\Entity\Skill;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -16,9 +17,25 @@ use Symfony\Component\HttpFoundation\Response;
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    
     public function index(): Response
     {
+        $nbMessage = $this->entityManager->getRepository(Contact::class)->count([]);
+        $nbProjet = $this->entityManager->getRepository(Projet::class)->count([]);
+        $nbParcours = $this->entityManager->getRepository(Parcours::class)->count([]);
+        $nbSkill = $this->entityManager->getRepository(Skill::class)->count([]);
+
         return $this->render('admin/my-dashboard.html.twig', [
+            'nbMessage' => $nbMessage,
+            'nbProjet' => $nbProjet,
+            'nbParcours' => $nbParcours,
+            'nbSkill' => $nbSkill
         ]);
     }
 
@@ -35,10 +52,10 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Présentation', 'fas fa-list', Presentation::class);
-        yield MenuItem::linkToCrud('Skills', 'fas fa-list', Skill::class);
-        yield MenuItem::linkToCrud('Parcours', 'fas fa-list', Parcours::class);
-        yield MenuItem::linkToCrud('Messages', 'fas fa-list', Contact::class);
-        yield MenuItem::linkToCrud('Mes projets', 'fas fa-list', Projet::class);
+        yield MenuItem::linkToCrud('Présentation', 'fas fa-user', Presentation::class);
+        yield MenuItem::linkToCrud('Mes projets', 'fas fa-briefcase', Projet::class);
+        yield MenuItem::linkToCrud('Parcours', 'fas fa-graduation-cap', Parcours::class);
+        yield MenuItem::linkToCrud('Skills', 'fas fa-lightbulb', Skill::class);
+        yield MenuItem::linkToCrud('Messages', 'fas fa-envelope', Contact::class);
     }
 }
