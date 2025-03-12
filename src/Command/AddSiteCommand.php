1dfbfcf0b05f112836689659ca5b9599f19b7685
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2025-03-12 09:12:29 +0100

    Retrait des use inutiles

--- src/Command/AddSiteCommand.php
+++ src/Command/AddSiteCommand.php
@@ -9,7 +9,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsCommand(
     name: 'app:add-site',
